2734c37657cb162636db109fe778f8b4df3bdc11
João Guilherme <79888134+joaoguicardosoo@users.noreply.github.com>
2022-07-25 01:27:01 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,15 @@
+// const = imutável
+// let = mutável
+
+
+const nome = 'joao';
+const sobrenome = 'guilherme';
+const idade = 21;
+const peso = 62;
+const alturaemM = 1.71;
+
+
+let imc = peso / (alturaemM**2);
+
+console.log(nome, sobrenome, 'possui', imc, 'de IMC.');
+console.log(`${nome} ${sobrenome} possui ${imc} de IMC.`);
